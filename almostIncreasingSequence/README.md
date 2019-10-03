520a93c152143d0a45290cd82fb5b1d4c7f0e584
keff6 <kev.fallas@gmail.com>
2019-10-03 13:46:52 -0600

    100AlgorithmsChallenge almostIncreasingSequence

--- almostIncreasingSequence/README.md
+++ almostIncreasingSequence/README.md
@@ -1,6 +1,3 @@
-### Check Out My [YouTube Channel](https://www.YouTube.com/CodingTutorials360)
-
----
 Given a sequence of integers as an array, determine whether it is possible to obtain a strictly increasing sequence by removing no more than one element from the array.
 
 **Example**
--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,5 +1,17 @@
 function almostIncreasingSequence(sequence: number[]): boolean {
+  let lastIncreasing = sequence[0]
+  let toRemoveCount = 0;
 
+  for(let i = 1; i < sequence.length - 1; i++) {
+    if(sequence[i] >= sequence[i+1] || lastIncreasing >= sequence[i]) {
+      toRemoveCount++
+      if(toRemoveCount > 1) return false
+    } else {
+      lastIncreasing = sequence[i]
+    }
+  }
+
+  return true;
 }
 
 // console.log(almostIncreasingSequence([1, 3, 2, 1])) 
