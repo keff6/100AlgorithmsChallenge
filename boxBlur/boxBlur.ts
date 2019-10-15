dd25d1b535504c805f79aaacfa2512183ed78bb3
keff6 <kev.fallas@gmail.com>
2019-10-14 18:26:57 -0600

    100AlgorithmsChallenge boxBlur complete

--- boxBlur/boxBlur.ts
+++ boxBlur/boxBlur.ts
@@ -1,26 +1,26 @@
 function boxBlur(image: number[][]): number[][] {
-const widthLen = image[0].length - 3
-  const heightLen = image.length - 3
-  const result = []
+  const widthLen = image[0].length - 3;
+  const heightLen = image.length - 3;
+  const result = [];
   
   for(let i=0; i <= widthLen; i++) {
-    const currentPixel = []
-    
+    const currentPixel = [];
     
     for(let j = 0; j <= heightLen; j++) {
-      const currentMatrix = image.slice(j,j+3).map(line => line.slice(j,j+3))
-    
-    console.log(currentMatrix)
-      
-     const valuesFromMatrix = []
-     currentMatrix.forEach(val => valuesFromMatrix.push(...val))
-     const pixel = Math.floor(valuesFromMatrix.reduce((acum, curr) => acum += curr) / 9)
-     currentPixel.push(pixel)
+      const currentMatrix = image.slice(i,i+3).map(line => line.slice(j,j+3));
+      const pixel = getPixel(currentMatrix);
+      currentPixel.push(pixel);
     }
-    result.push(currentPixel)
+    result.push(currentPixel);
   }
   
-  return result
+  return result;
+}
+
+function getPixel(matrix) {
+  const valuesFromMatrix = [];
+  matrix.forEach(val => valuesFromMatrix.push(...val));
+  return Math.floor(valuesFromMatrix.reduce((acum, curr) => acum += curr) / 9);
 }
 
 console.log(boxBlur([[1, 1, 1], 
