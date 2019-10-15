9a0a4733f777865cf6e50bb42459ee8872f6e6b3
keff6 <kev.fallas@gmail.com>
2019-10-14 18:11:24 -0600

    100AlgoritmCHallenge boxBlur in progress

--- boxBlur/README.md
+++ boxBlur/README.md
@@ -1,7 +1,3 @@
-### Check Out My [YouTube Channel](https://www.YouTube.com/CodingTutorials360)
-
-##### Algorithm Challenge Available At [CodeFights](https://codefights.com/arcade/intro/level-5/5xPitc3yT3dqS7XkP)
----
 Last night you had to study, but decided to party instead. Now there is a black and white photo of you that is about to go viral. You cannot let this ruin your reputation, so you want to apply box blur algorithm to the photo to hide its content.
 
 The algorithm works as follows: each pixel x in the resulting image has a value equal to the average value of the input image pixels' values from the 3 × 3 square with the center at x. All pixels at the edges are cropped.
--- boxBlur/boxBlur.ts
+++ boxBlur/boxBlur.ts
@@ -1,5 +1,26 @@
 function boxBlur(image: number[][]): number[][] {
-
+const widthLen = image[0].length - 3
+  const heightLen = image.length - 3
+  const result = []
+  
+  for(let i=0; i <= widthLen; i++) {
+    const currentPixel = []
+    
+    
+    for(let j = 0; j <= heightLen; j++) {
+      const currentMatrix = image.slice(j,j+3).map(line => line.slice(j,j+3))
+    
+    console.log(currentMatrix)
+      
+     const valuesFromMatrix = []
+     currentMatrix.forEach(val => valuesFromMatrix.push(...val))
+     const pixel = Math.floor(valuesFromMatrix.reduce((acum, curr) => acum += curr) / 9)
+     currentPixel.push(pixel)
+    }
+    result.push(currentPixel)
+  }
+  
+  return result
 }
 
 console.log(boxBlur([[1, 1, 1], 
