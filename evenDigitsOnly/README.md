1ba6d5298c5822f01d347676ca55d88a0e7334d5
keff6 <kev.fallas@gmail.com>
2019-10-28 10:37:02 -0600

    100AlgorithmsChallenge evenDigitsOnly

--- evenDigitsOnly/README.md
+++ evenDigitsOnly/README.md
@@ -1,7 +1,3 @@
-### Check Out My [YouTube Channel](https://www.YouTube.com/CodingTutorials360)
-
-##### Algorithm Challenge Available At [CodeFights](https://codefights.com/arcade/intro/level-6/6cmcmszJQr6GQzRwW)
----
 Check if all digits of the given integer are even.
 
 **Example**
--- evenDigitsOnly/evenDigitsOnly.ts
+++ evenDigitsOnly/evenDigitsOnly.ts
@@ -1,6 +1,19 @@
 function evenDigitsOnly(n: number): boolean {
+  while(n > 0) {
+    const digit = n % 10
+    if(digit % 2 !== 0) return false
+    n = Math.trunc(n /10)
+  }
+  return true
 
+  /*
+  Option B
+  const notEven = n.toString().split('').map(Number).filter(val => val % 2 !== 0)
+  return notEven.length > 0
+  */
 }
+
+// Big O:  O(log n)
 
 console.log(evenDigitsOnly(248622));
 console.log(evenDigitsOnly(642386));
