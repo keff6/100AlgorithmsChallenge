87a73360353674d739b43f60480226d9a5a8ff34
keff6 <kev.fallas@gmail.com>
2019-11-05 08:16:29 -0600

    100AlgorithmsChallenge firstNotRepeatingCharacter

--- firstNotRepeatingCharacter/README.md
+++ firstNotRepeatingCharacter/README.md
@@ -1,6 +1,3 @@
-### Check Out My [YouTube Channel](https://www.YouTube.com/CodingTutorials360)
-
-##### Algorithm Challenge Available At [CodeFights](https://codefights.com/interview-practice/task/uX5iLwhc6L5ckSyNC)
 Note: Write a solution that only iterates over the string once and uses O(1) additional memory, since this is what you would be asked to do during a real interview.
 
 Given a string s, find and return the first instance of a non-repeating character in it. If there is no such character, return '_'.
--- firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
+++ firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
@@ -1,6 +1,22 @@
 function firstNotRepeatingCharacter(s: string): string {
-
+  const dict = {}
+  
+  for(let i = 0; i < s.length; i++) {
+    const currenChar = s[i];
+    
+    if(dict[currenChar]) {
+      dict[currenChar] = -1
+    } else {
+      dict[currenChar] = i
+    }
+  }
+  
+  const minIndex = Object.values(dict).filter(v => v !== -1)[0]
+  
+  return s[minIndex] || '_'
 }
 
 console.log(firstNotRepeatingCharacter('abacabad'));
 console.log(firstNotRepeatingCharacter('abacabaabacaba'));
+
+// Search for a more optimal solution
