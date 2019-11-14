f59779318b7777c9ffbdd5b5f7fe87a6cc17b964
keff6 <kev.fallas@gmail.com>
2019-11-14 11:53:58 -0600

    100AlgorithmsChallenge longestDigitsPrefix

--- longestDigitsPrefix/longestDigitsPrefix.ts
+++ longestDigitsPrefix/longestDigitsPrefix.ts
@@ -1,5 +1,16 @@
 function longestDigitsPrefix(inputString: string): string {
-
+  let result = []
+  const inputArr = inputString.split('');
+  
+  for(let char of inputArr){
+    if(Number(char)) {
+      result.push(char)
+    } else {
+      break;
+    }
+  }
+  
+  return result.join('')
 }
 
 console.log(longestDigitsPrefix('123aa1'));
