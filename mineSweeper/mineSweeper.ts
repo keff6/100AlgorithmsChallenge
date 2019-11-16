00c7f433405d89d49113134b4fd05c4f17e93bec
keff6 <kev.fallas@gmail.com>
2019-11-16 15:04:33 -0600

    100AlgorithmsChallenge mineSweeper for pyhton

--- mineSweeper/mineSweeper.ts
+++ mineSweeper/mineSweeper.ts
@@ -45,3 +45,52 @@ function minesweeper(matrix: boolean[][]): number[][] {
 console.log(minesweeper([[true, false, false],
 [false, true, false],
 [false, false, false]]));
+
+/*
+Pyhton
+
+def minesweeper(matrix):
+  lenX = len(matrix[0]) - 1
+  lenY = len(matrix) - 1
+  resultMatrix = []
+
+  for i,row in enumerate(matrix):
+    currentRow = []
+    for j,col in enumerate(row):
+      neighbors = 0
+
+      # Count 8 neighbors
+      # top 3 elments
+      
+      if i > 0 and j > 0 and matrix[i - 1][j - 1] == True:
+        neighbors += 1
+      if j < lenX and i > 0 and matrix[i - 1][j + 1] == True:
+        neighbors += 1
+      if i > 0 and matrix[i - 1][j] == True:
+        neighbors += 1
+      
+      # left and right 
+      if j > 0 and matrix[i][j - 1] == True:
+        neighbors += 1
+      if j < lenX and matrix[i][j+1] == True:
+        neighbors += 1
+      
+      # bottom 3 elements
+      if i < lenY and matrix[i + 1][j] == True:
+        neighbors += 1
+      
+      if i < lenY and j < lenX and matrix[i+1][j+1] == True:
+        neighbors += 1
+      if i < lenY and j > 0 and matrix[i + 1][j-1] == True:
+        neighbors += 1
+      currentRow.append(neighbors)
+
+    resultMatrix.append(currentRow)
+  
+  return resultMatrix
+
+
+print(minesweeper([[True, False, False],
+                  [False, True, False],
+                  [False, False, False]]));
+*/
