4fcdb31a1a8c66bdc9bacd39004008a558ebe387
keff6 <kev.fallas@gmail.com>
2019-11-18 11:26:09 -0600

    100AlgorithmsChallenge minimalNumberOfCoins

--- minimalNumberOfCoins/README.md
+++ minimalNumberOfCoins/README.md
@@ -1,7 +1,3 @@
-### Check Out My [YouTube Channel](https://www.YouTube.com/CodingTutorials360)
-
-##### Algorithm Challenge Available At [CodeFights](https://codefights.com/arcade/intro/level-5/ZMR5n7vJbexnLrgaM)
----
 You find yourself in Bananaland trying to buy a banana. You are super rich so you have an unlimited supply of banana-coins, but you are trying to use as few coins as possible.
 
 The coin values available in Bananaland are stored in a sorted array coins. coins[0] = 1, and for each i (0 &lt; i &lt; coins.length) coins[i] is divisible by coins[i - 1]. Find the minimal number of banana-coins you'll have to spend to buy a banana given the banana's price.
--- minimalNumberOfCoins/minimalNumberOfCoins.ts
+++ minimalNumberOfCoins/minimalNumberOfCoins.ts
@@ -1,5 +1,34 @@
 function minimalNumberOfCoins(coins: number[], price: number): number {
-
+  let coinsCount = 0;
+  let index = coins.length - 1
+  
+  while(price !== 0 || index < 0) {
+    coinsCount += Math.floor(price / coins[index])
+    price = price % coins[index]
+    index--
+  }
+  
+  return coinsCount
 }
 
+// O(n)
+
 console.log(minimalNumberOfCoins([1, 2, 10], 28));
+
+/**
+ * Python
+import math
+
+def minimalNumberOfCoins(coins, price):
+  coinsCount = 0
+  index = len(coins) - 1
+  
+  while price != 0 or index < 0:
+    coinsCount += math.floor(price / coins[index])
+    price = price % coins[index]
+    index -= 1
+  
+  return coinsCount
+
+print(minimalNumberOfCoins([1, 2, 10], 28))
+ */
