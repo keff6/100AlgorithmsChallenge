602d1ce83bda444d36b5fad9d9d0129e3e56d727
keff6 <kev.fallas@gmail.com>
2019-11-20 09:41:21 -0600

    100AlgorithmsChallenge newNumeralSystem

--- newNumeralSystem/README.md
+++ newNumeralSystem/README.md
@@ -1,7 +1,3 @@
-### Check Out My [YouTube Channel](https://www.YouTube.com/CodingTutorials360)
-
-##### Algorithm Challenge Available At [CodeFights](https://codefights.com/arcade/intro/level-5/ZMR5n7vJbexnLrgaM)
----
 Your Informatics teacher at school likes coming up with new ways to help you understand the material. When you started studying numeral systems, he introduced his own numeral system, which he's convinced will help clarify things. His numeral system has base 26, and its digits are represented by English capital letters - A for 0, B for 1, and so on.
 
 The teacher assigned you the following numeral system exercise: given a one-digit number, you should find all unordered pairs of one-digit numbers whose values add up to the number.
--- newNumeralSystem/newNumeralSystem.ts
+++ newNumeralSystem/newNumeralSystem.ts
@@ -1,5 +1,43 @@
 function newNumeralSystem(number: string): string[] {
+  const alphabeth = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  let index = alphabeth.indexOf(number.toUpperCase())
+  const result = []
+  
+  if(index !== -1) {
+    const half = Math.floor(index / 2)
+    let complement = 0
+    
+    while(index > half) {
+      const currentStr = `${alphabeth[complement]} + ${alphabeth[index]}`
+      result.push(currentStr)
+      complement++
+      index--
+    }
+  }
 
+  return result
 }   
 
 console.log(newNumeralSystem('G'));
+
+/**
+ * Python
+def newNumeralSystem(number):
+  alphabeth = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  index = alphabeth.find(number.upper())
+  result = []
+  
+  if index != -1:
+    half = round(index / 2, 0)
+    complement = 0
+    
+    while complement <= half:
+      currentStr = f'{alphabeth[complement]} + {alphabeth[index]}'
+      result.append(currentStr)
+      complement += 1
+      index -= 1
+
+  return result
+
+print(newNumeralSystem('I'));
+ */
