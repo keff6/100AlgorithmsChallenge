46640fd25650ba424337ce17fcec3f3761bea2ab
keff6 <kev.fallas@gmail.com>
2019-11-24 13:38:13 -0600

    100AlgorithmsChallenge pigLatin

--- pigLatin/README.md
+++ pigLatin/README.md
@@ -1,7 +1,3 @@
-### Check Out My [YouTube Channel](https://www.YouTube.com/CodingTutorials360)
-
-### Check Out More Algorithms like this at <a href="https://www.FreeCodeCamp.com"> FreeCodeCamp</a>
----
 Translate the provided string to pig latin.
 
 Pig Latin takes the first consonant (or consonant cluster) of an English word, moves it to the end of the word and suffixes an "ay".
--- pigLatin/pigLatin.ts
+++ pigLatin/pigLatin.ts
@@ -1,5 +1,22 @@
 function pigLatin(str: string): string {
+  const vowels = 'aeiou'
+  const sufix = []
+  
+  if(str.length > 0 && vowels.includes(str[0])) {
+    return str.concat('way')
+  }
 
+  let splitIndex = 0
+  for(let i =0 ; i< str.length; i++) {
+    if(vowels.includes(str[i])) {
+      splitIndex = i
+      break
+    } else {
+      sufix.push(str[i])
+    }
+  }
+  
+  return str.substr(splitIndex).concat(sufix.join('').concat('ay'))
 }
 
 console.log(pigLatin("glove"));
