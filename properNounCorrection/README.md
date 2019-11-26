bc5cf8ffa9ee62de8b06a7ec897c9a506cd98561
keff6 <kev.fallas@gmail.com>
2019-11-26 08:51:35 -0600

    100AlgorithmsChallenge properNounCorrection

--- properNounCorrection/README.md
+++ properNounCorrection/README.md
@@ -1,7 +1,3 @@
-### Check Out My [YouTube Channel](https://www.YouTube.com/CodingTutorials360)
-
-##### Algorithm Challenge Available At [CodeFights](https://codefights.com/arcade/code-arcade/book-market/2nSEQ8CGoddTJtnbo)
----
 Proper nouns always begin with a capital letter, followed by small letters.
 
 Correct a given proper noun so that it fits this statement.
--- properNounCorrection/properNounCorrection.ts
+++ properNounCorrection/properNounCorrection.ts
@@ -1,6 +1,19 @@
 function properNounCorrection(noun: string): string {
-   
+  return noun[0]
+          .toUpperCase()
+          .concat(noun.substr(1).toLowerCase())
 }
 
 console.log(properNounCorrection('pARiS'));
 console.log(properNounCorrection('John'));
+
+/*
+python
+
+def properNounCorrection(noun):
+  return noun[0].upper() + noun[1:].lower()
+
+
+print(properNounCorrection('pARiS'));
+print(properNounCorrection('John'));
+*/
