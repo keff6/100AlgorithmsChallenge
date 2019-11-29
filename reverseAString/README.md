510fbae555ae486a455628cd04542df06cdaf022
keff6 <kev.fallas@gmail.com>
2019-11-29 14:59:49 -0600

    100AlgorithmsChallenge reverseAString

--- reverseAString/README.md
+++ reverseAString/README.md
@@ -1,8 +1,3 @@
-### Check Out My [YouTube Channel](https://www.YouTube.com/CodingTutorials360)
-
-### Check Out More Algorithms like this at <a href="https://www.FreeCodeCamp.com"> FreeCodeCamp</a>
----
-
 Reverse the provided string.
 
 You may need to turn the string into an array before you can reverse it.
--- reverseAString/reverseAString.ts
+++ reverseAString/reverseAString.ts
@@ -1,6 +1,32 @@
 function reverseAString(str: string): string {
-
+  const reversed = []
+  const strArr = str.split('')
+  
+  while(strArr.length > 0) {
+    reversed.push(strArr.pop())
+  }
+  return reversed.join('') 
+  // Optional solution:
+  // return str.split('').reverse().join('')
 }
 
 console.log(reverseAString('hello'));
 console.log(reverseAString('Howdy'));
+
+/*
+python
+
+def reverseAString(str):
+  reversed = ''
+  
+  for i in range(len(str) - 1, -1, -1):
+    reversed += str[i]
+
+  return reversed
+
+  # optional
+  # return str[::-1]
+
+print(reverseAString('hello'));
+print(reverseAString('Howdy'));
+*/
