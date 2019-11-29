80406115a81ce811ed587d6f54d410f77eb7d941
keff6 <kev.fallas@gmail.com>
2019-11-29 11:07:01 -0600

    100AlgorithmsChallenge reflectString

--- reflectString/README.md
+++ reflectString/README.md
@@ -1,8 +1,3 @@
-### Check Out My [YouTube Channel](https://www.YouTube.com/CodingTutorials360)
-
-##### Algorithm Challenge Available At [CodeFights](https://codefights.com/arcade/code-arcade/lab-of-transformations/8nAgfjhDvKCpxwGWF)
----
-
 Define an alphabet reflection as follows: a turns into z, b turns into y, c turns into x, ..., n turns into m, m turns into n, ..., z turns into a.
 
 Define a string reflection as the result of applying the alphabet reflection to each of its characters.
--- reflectString/reflectString.ts
+++ reflectString/reflectString.ts
@@ -1,5 +1,30 @@
 function reflectString(inputString: string): string {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  let reflexedString = ''
   
+  for(let i = 0; i < inputString.length; i++) {
+    const reflectIndex = alphabet.indexOf(inputString[i].toLowerCase()) + 1
+    reflexedString += alphabet.substr(-reflectIndex,1)
+  }
+  
+  return reflexedString
 }
 
 console.log(reflectString("name"));
+
+/*
+python
+
+def reflectString(inputString):
+  alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  reflexedString = ''
+  
+  for char in inputString:
+    reflectIndex = alphabet.index(char.lower()) + 1
+    reflexedString += alphabet[-reflectIndex]
+  
+  return reflexedString
+
+
+print(reflectString("name"));
+*/
