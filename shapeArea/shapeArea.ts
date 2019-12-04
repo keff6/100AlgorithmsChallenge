ff6c3a15bd8ac22239641d8b4822b4027fb4187e
keff6 <kev.fallas@gmail.com>
2019-12-04 10:55:14 -0600

    100AlgorithmsChallenge shapeArea

--- shapeArea/shapeArea.ts
+++ shapeArea/shapeArea.ts
@@ -1,5 +1,22 @@
 function shapeArea(n: number): number {
+  // my solution
+  if(n === 1) {
+    return 1
+  }
+  else {
+    return shapeArea(n -1) + (n * 2 + ((n - 2) * 2))
+  }
+  /*
+  best solution
 
+  let area = 1
+
+  for(let i = 1; i< n; i++){
+    area += 4 * i
+  }
+
+  return area
+  */
 }
 
 console.log(shapeArea(2));
