88f105e2f193be5d61aeff145b9c6fda5a8f75d0
keff6 <kev.fallas@gmail.com>
2020-01-02 12:08:09 -0600

    100AlgorithmsChallenge validTime

--- validTime/README.md
+++ validTime/README.md
@@ -1,7 +1,3 @@
-### Check Out My [YouTube Channel](https://www.YouTube.com/CodingTutorials360)
-
-##### Algorithm Challenge Available At [CodeFights](https://codefights.com/arcade/code-arcade/time-river/ywMyCTspqGXPWRZx5)
----
 Check if the given string is a correct time representation of the 24-hour clock.
 
 **Example**
--- validTime/validTime.ts
+++ validTime/validTime.ts
@@ -1,7 +1,22 @@
 function validTime(time: string): boolean {
-
+  const [hours, seconds] = time.split(':').map(Number)
+  return (hours < 24 && hours > 0) && (seconds < 60 && seconds > 0)
 }
 
 console.log(validTime('13:58'));
 console.log(validTime('25:51'));
 console.log(validTime('02:76'));
+
+/*
+python
+
+def validTime(time):
+  [hours, seconds] = list(map(lambda n: int(n),time.split(':')))
+  return (hours < 24 and hours > 0) and (seconds < 60 and seconds > 0)
+
+print(validTime('13:58'));
+print(validTime('25:51'));
+print(validTime('02:76'));
+
+
+*/
